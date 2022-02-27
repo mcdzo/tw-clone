ea40c6704ed0685934f2fc2396dd245ee3e5bdd2
Maximiliano Cardozo <maximilianocardozo710@gmail.com>
2022-02-27 15:01:10 -0300

    delete code comments

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -26,35 +26,3 @@ const verifyToken = (req, res, next) => {
 };
 module.exports = verifyToken;
 
-/*
-
-const jwt = require("jsonwebtoken");
-
-require("dotenv").config();
-const config = process.env;
-
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const params = req.body;
-  console.log(params);
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
-
-  if (!token) {
-    return res.status(403).send("Se necesita un token para autenticarse");
-  }
-  try {
-    const decoded = jwt.verify(token, config.TOKEN_KEY);
-    console.log(decoded);
-    req.user = decoded;
-    console.log(req.user);
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
-};
-module.exports = verifyToken;
-
-
-
-*/
